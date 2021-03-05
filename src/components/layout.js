c4cebfa87ce712b103d0db603e15edc96ca465bd
Smail KOURTA <gs_kourta@esi.dz>
2021-03-05 18:55:39 +0100

    speakers section

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,13 +26,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
         <footer
           style={{
