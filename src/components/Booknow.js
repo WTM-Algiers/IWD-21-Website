93f461504effe7f4e288e3fbe9567309d2c0ee8b
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2021-03-06 15:48:32 +0100

    fml

---
+++ src/components/Booknow.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Booknow extends Component {
+  render() {
+    return (
+      <a href="https:\\www.google.com" target="_blank" className="booknow">
+        Book Now!
+      </a>
+    )
+  }
+}
+
+export default Booknow
--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react"
 import Countdown from "./Countdown"
 import Descreption from "./Descreption"
+import Booknow from "./Booknow"
 
 class HomeSection extends Component {
   render() {
     return (
       <div className="homesection">
+        <Booknow></Booknow>
         <Descreption></Descreption>
         <Countdown futureDate="2021-04-18 00:00:00"></Countdown>
       </div>
