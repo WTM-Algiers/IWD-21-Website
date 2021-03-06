af698f91c0a4cec2181290494e58b9a00ffbda39
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2021-03-06 14:52:25 +0100

    fix problems

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,20 @@ import SEO from "../components/seo"
 import HomeSection from "../components/HomeSection"
 
 const IndexPage = () => (
-  <Layout>
-    <HomeSection></HomeSection>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <>
+    <Layout>
+      <HomeSection></HomeSection>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  </>
 )
 
 export default IndexPage
