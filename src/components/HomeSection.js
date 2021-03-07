b8cf078dc5181e22e183ff930f4a409fccebb5db
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2021-03-07 11:42:19 +0100

    add women background hero nd fix responsivité

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -2,14 +2,20 @@ import React, { Component } from "react"
 import Countdown from "./Countdown"
 import Descreption from "./Descreption"
 import Booknow from "./Booknow"
-
+import hero from "../images/women-hero.png"
 class HomeSection extends Component {
   render() {
     return (
       <div className="homesection">
-        <Booknow></Booknow>
-        <Descreption></Descreption>
-        <Countdown futureDate="2021-04-18 00:00:00"></Countdown>
+        <div className="lefthero">
+          <Descreption></Descreption>
+          <Countdown futureDate="2021-04-18 00:00:00"></Countdown>
+        </div>
+
+        <div className="imagehero">
+          <Booknow></Booknow>
+          <img src={hero} className="womenhero"></img>
+        </div>
       </div>
     )
   }
--- src/components/layout.js
+++ src/components/layout.js
@@ -30,7 +30,6 @@ const Layout = ({ children }) => {
         style={{
           margin: `0 auto`,
           maxWidth: "100%",
-          padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
