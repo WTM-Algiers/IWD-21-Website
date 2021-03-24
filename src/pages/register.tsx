1303425ce361da0ecf4896df92124cd6747f73c7
Redha Amirouche <hm_amirouche@esi.dz>
2021-03-24 02:37:22 +0100

    fix build issue - replace form component with Iframe

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,37 +1,15 @@
-import React, { ReactElement } from "react"
-import { run } from "tripetto-runner-autoscroll"
-import Services from "tripetto-services"
+import React from "react"
 
-function Register(): ReactElement {
-  const {
-    definition,
-    styles,
-    l10n,
-    locale,
-    translations,
-    snapshot,
-    attachments,
-    onSubmit,
-    onPause,
-  } = Services.init({
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoibmdkSnNFMFFNdlFsMjQramZsd1Z2WTlsMU1xZWRMOWFONzBaUGVlRjZjMD0iLCJkZWZpbml0aW9uIjoiTFZveFMxWTBPL1ZkaWxtei9kYzdTcWFRTFI0WlRHTENLRDVSQUJ1V3huWT0iLCJ0eXBlIjoiY29sbGVjdCJ9.RjMera1tB9mOfU5Z3kuTM3G6j0c8U7FH4rgVsNBCg7E",
-  })
-
-  run({
-    element: document.body /* Or supply your own element here */,
-    definition,
-    styles,
-    l10n,
-    locale,
-    translations,
-    attachments,
-    onSubmit,
-    snapshot,
-    onPause,
-    persistent: true,
-  })
-
-  return <></>
+function Register() {
+  return (
+    <>
+      <iframe
+        src="https://tripetto.app/run/T06ISDL4E9"
+        className="h-screen w-full"
+        frameborder="0"
+      ></iframe>
+    </>
+  )
 }
+
 export default Register
